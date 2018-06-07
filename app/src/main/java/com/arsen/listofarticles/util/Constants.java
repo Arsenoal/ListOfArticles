31fe4691b31c0b3bb079b08badaf3db2b3e4c09c
Arsen <arsen.budumyan@gmail.com>
2018-06-07 18:36:08 +0400

    key added

--- app/src/main/java/com/arsen/listofarticles/util/Constants.java
+++ app/src/main/java/com/arsen/listofarticles/util/Constants.java
@@ -4,4 +4,5 @@ public final class Constants {
     private Constants(){}
 
     public final static String BASE_URL = "https://content.guardianapis.com/search";
+    public final static String API_KEY = "5c4394bd-ee88-436a-a151-b1e9a26dbc41";
 }
